2f52dd3cf9c937f36b78899bd410a1b878f6238d
Albert-Asadov <albertma@code.edu.az>
2023-03-23 21:07:41 +0400

    Pronia

---
+++ P230_Pronia/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+using P230_Pronia.DAL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ProniaDbContext>(opt =>
+{
+    opt.UseSqlServer("server = localhost; database = Pronia; user Id = SA; password = Albert526495");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ P230_Pronia/ViewModel/HomeVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using P230_Pronia.Entities;
+
+namespace P230_Pronia.ViewModel
+{
+	public class HomeVM
+	{
+		public List<Slider> slider { get; set; }
+
+		public List<Plant> plant { get; set; }
+	}
+}
+
